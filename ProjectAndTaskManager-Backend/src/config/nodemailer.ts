372c2bdc3dc08d1c4d566b6a9f6f66bcf02eec25
Sebastian Galindo Zaragoza <133563476+Sebastian-Zaragoza@users.noreply.github.com>
2025-12-19 13:57:07 -0600

    f: files updated for production deployment

--- ProjectAndTaskManager-Backend/src/config/nodemailer.ts
+++ ProjectAndTaskManager-Backend/src/config/nodemailer.ts
@@ -1,51 +1,25 @@
-import nodemailer from "nodemailer";
-import { google } from "googleapis";
+import { Resend } from 'resend';
 import dotenv from "dotenv";
 dotenv.config();
-
-const {
-    GMAIL_OAUTH_CLIENT_ID,
-    GMAIL_OAUTH_CLIENT_SECRET,
-    GMAIL_OAUTH_REFRESH_TOKEN,
-    GMAIL_USER
-} = process.env!;
-
-const oAuth2Client = new google.auth.OAuth2(
-    GMAIL_OAUTH_CLIENT_ID!,
-    GMAIL_OAUTH_CLIENT_SECRET!,
-    "https://developers.google.com/oauthplayground"
-);
-oAuth2Client.setCredentials({ refresh_token: GMAIL_OAUTH_REFRESH_TOKEN! });
-
-async function createTransporter() {
-    const { token } = await oAuth2Client.getAccessToken();
-    return nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-            type: "OAuth2",
-            user: GMAIL_USER!,
-            clientId: GMAIL_OAUTH_CLIENT_ID!,
-            clientSecret: GMAIL_OAUTH_CLIENT_SECRET!,
-            refreshToken: GMAIL_OAUTH_REFRESH_TOKEN!,
-            accessToken: token!,
-        },
-    });
-}
-
+const resend = new Resend(process.env.RESEND_API_KEY);
 export async function sendEmail(opts: {
     to: string;
     subject: string;
     html: string;
     text?: string;
 }) {
-    const transporter = await createTransporter();
-
-    const info = await transporter.sendMail({
-        from: `"DevFlow" <${GMAIL_USER}>`,
-        to: opts.to,
-        subject: opts.subject,
-        text: opts.text,
-        html: opts.html,
-    });
-    return info;
+    try {
+        const data = await resend.emails.send({
+            from: 'DevFlow <onboarding@resend.dev>',
+            to: opts.to,
+            subject: opts.subject,
+            html: opts.html,
+            text: opts.text || '',
+        });
+        console.log("Email send via Resend");
+        return data;
+    } catch (error) {
+        console.error("Error trying to sent a email via Resend:", error);
+        throw error;
+    }
 }
